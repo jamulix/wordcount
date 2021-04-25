a5137ecb24a2f8348f72490023b0e0d0aa59dc7e
Dieter Schlüter <dieter.schlueter@linix.de>
2021-04-25 06:50:45 +0200

    Texteingabe ermöglicht

--- word_count.py
+++ word_count.py
@@ -1,4 +1,6 @@
-happy = "I felt happy because I saw the others were happy and because I knew I should feel happy but I was not really happy"
+# happy = "I felt happy because I saw the others were happy and because I knew I should feel happy but I was not really happy"
+
+happy = input()
 
 words = happy.split()
 
