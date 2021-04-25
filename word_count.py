a279c1b23948af79fe3a54a611f89ed4271ccf5c
Jamulix <80723001+jamulix@users.noreply.github.com>
2021-04-25 07:40:08 +0200

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -1,4 +1,4 @@
-# happy = "I felt happy because I saw the others were happy and because I knew I should feel happy but I was not really happy"
+# Dummy Program just for Testing GitHub
 
 print "Enter a statement to word count: "
 happy = input()
